bd59f012138a0d7519526fe6fa9668ca60542f29
farhanmanna <farhanrizwan123@gmail.com>
2020-07-24 17:12:49 +0530

    Add backslash in routes

--- routes/web.php
+++ routes/web.php
@@ -25,10 +25,10 @@ $router->group([ 'prefix' => '/tenant-module' ], function () use ($router) {
 // TENANT ROUTES
 $router->group([ 'prefix' => '/tenant' ], function () use ($router) {
 
-    $router->post('create', 'TenantController@create');
-    $router->post('update', 'TenantController@update');
-    $router->get('list', 'TenantController@list');
-    $router->get('show/{id}', 'TenantController@show');
-    $router->post('delete', 'TenantController@delete');
+    $router->post('/create', 'TenantController@create');
+    $router->post('/update', 'TenantController@update');
+    $router->get('/list', 'TenantController@list');
+    $router->get('/show/{id}', 'TenantController@show');
+    $router->post('/delete', 'TenantController@delete');
     
 });
