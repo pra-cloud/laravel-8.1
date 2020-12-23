7e6aa7f7d1b9776266cf254a8173ab139f931a88
Gulzar Ahmed <alignwebs@gmail.com>
2020-12-24 04:17:03 +0530

    Added Prefix TENANT_DB

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('TENANT_DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -35,48 +35,48 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => env('DB_PREFIX', ''),
+            'database' => env('TENANT_DB_DATABASE', database_path('database.sqlite')),
+            'prefix' => env('TENANT_DB_PREFIX', ''),
         ],
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', 3306),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => env('DB_CHARSET', 'utf8mb4'),
-            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
-            'prefix' => env('DB_PREFIX', ''),
-            'strict' => env('DB_STRICT_MODE', true),
-            'engine' => env('DB_ENGINE', 'InnoDB'),
-            'timezone' => env('DB_TIMEZONE', '+00:00'),
+            'host' => env('TENANT_DB_HOST', '127.0.0.1'),
+            'port' => env('TENANT_DB_PORT', 3306),
+            'database' => env('TENANT_DB_DATABASE', 'forge'),
+            'username' => env('TENANT_DB_USERNAME', 'forge'),
+            'password' => env('TENANT_DB_PASSWORD', ''),
+            'unix_socket' => env('TENANT_DB_SOCKET', ''),
+            'charset' => env('TENANT_DB_CHARSET', 'utf8mb4'),
+            'collation' => env('TENANT_DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => env('TENANT_DB_PREFIX', ''),
+            'strict' => env('TENANT_DB_STRICT_MODE', true),
+            'engine' => env('TENANT_DB_ENGINE', 'InnoDB'),
+            'timezone' => env('TENANT_DB_TIMEZONE', '+00:00'),
         ],
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', 5432),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => env('DB_PREFIX', ''),
-            'schema' => env('DB_SCHEMA', 'public'),
-            'sslmode' => env('DB_SSL_MODE', 'prefer'),
+            'host' => env('TENANT_DB_HOST', '127.0.0.1'),
+            'port' => env('TENANT_DB_PORT', 5432),
+            'database' => env('TENANT_DB_DATABASE', 'forge'),
+            'username' => env('TENANT_DB_USERNAME', 'forge'),
+            'password' => env('TENANT_DB_PASSWORD', ''),
+            'charset' => env('TENANT_DB_CHARSET', 'utf8'),
+            'prefix' => env('TENANT_DB_PREFIX', ''),
+            'schema' => env('TENANT_DB_SCHEMA', 'public'),
+            'sslmode' => env('TENANT_DB_SSL_MODE', 'prefer'),
         ],
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', 1433),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => env('DB_PREFIX', ''),
+            'host' => env('TENANT_DB_HOST', 'localhost'),
+            'port' => env('TENANT_DB_PORT', 1433),
+            'database' => env('TENANT_DB_DATABASE', 'forge'),
+            'username' => env('TENANT_DB_USERNAME', 'forge'),
+            'password' => env('TENANT_DB_PASSWORD', ''),
+            'charset' => env('TENANT_DB_CHARSET', 'utf8'),
+            'prefix' => env('TENANT_DB_PREFIX', ''),
         ],
 
     ],
