aeeea84b230e815119efbc0f41476877e0e0c8d2
Azam Siddiqui <siddiquiazam966@gmail.com>
2021-12-24 16:48:16 +0530

    fix

--- app/Modules/Billing/DataTransferObjects/CustomerDto.php
+++ app/Modules/Billing/DataTransferObjects/CustomerDto.php
@@ -6,10 +6,10 @@ use Spatie\DataTransferObject\DataTransferObject;
 
 class CustomerDTO extends DataTransferObject
 {
-    public string $customerId;
-    public string $email;
-    public string $firstName;
-    public string $lastName;
-    public string $city;
-    public string $country;
+   public string $customerId;
+   public string $email;
+   public string $firstName;
+   public string $lastName;
+   public string $city;
+   public string $country;
 }
