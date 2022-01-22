e96d6da0927406264fd32155280bd7fca9e87d63
Azam Siddiqui <siddiquiazam966@gmail.com>
2022-01-22 17:30:56 +0530

    mq updated

--- config/mq.php
+++ config/mq.php
@@ -1,13 +1,28 @@
 <?php
 
 return [
+  'connection' => [
+    'host' => env('AMQP_HOST', 'localhost'),
+    'port' => env('AMQP_PORT', 5672),
+    'user' => env('AMQP_USER', 'guest'),
+    'password' => env('AMQP_PASSWORD', 'guest'),
+    'vhost' => env('AMQP_VHOST', '/')
+  ],
   'queue_config' => [
     [
       'exchange' => 'tenant_exchange',
       'queues' => [
-        'tenant.updated.is_open' => \App\Modules\Mq\Callbacks\TenantOpenStatusUpdatedCallback::class,
-        'tenant.billing.subscribed' => \App\Modules\Mq\Callbacks\SubscribeTenantToSaasModule::class,
+        [
+          "name" => "tenant.open",
+          "binding_key" => "tenant.updated.open",
+          "callback" => \App\Modules\Mq\Callbacks\TenantOpenStatusUpdatedCallback::class,
+        ],
+        [
+          "name" => "tenant.billing",
+          "binding_key" => "tenant.billing.subscribed",
+          "callback" => \App\Modules\Mq\Callbacks\SubscribeTenantToSaasModule::class,
+        ],
       ]
-    ],
+    ]
   ]
 ];
