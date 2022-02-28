58ac3d7f772d32279b2614520f3d399d108c2739
Azam Siddiqui <siddiquiazam966@gmail.com>
2022-02-28 14:48:12 +0530

    fix

--- app/Modules/Mq/Callbacks/TenantOpenStatusUpdatedCallback.php
+++ app/Modules/Mq/Callbacks/TenantOpenStatusUpdatedCallback.php
@@ -10,7 +10,7 @@ class TenantOpenStatusUpdatedCallback
    {
       if (isset($data["tenant_id"]) && isset($data["is_open"])) {
          echo "Open status: $data[is_open] for tenant: $data[tenant_id]";
-         $tenant = Tenant::find($data["tenant_id"]);
+         $tenant = Tenant::find(intval($data["tenant_id"]));
          if ($tenant) {
             $tenant->is_open = $data["is_open"];
             $tenant->save();
