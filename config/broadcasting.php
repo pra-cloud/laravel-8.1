39b7da8dcc7bbd35ab9070f1ecca31d621d0df86
Gulzar Ahmed <alignwebs@gmail.com>
2022-05-12 22:46:00 +0530

    pusher

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,7 +37,11 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => true,
+                // 'useTLS' => true,
+                'encrypted' => true,
+                'host' => env('PUSHER_APP_HOST'),
+                'port' => env('PUSHER_APP_PORT'),
+                'scheme' => 'http'
             ],
         ],
 
