de50c1479e603bc6cadb6ade2232c4b0bb27bb3e
Prateek Mishra <64471391+pra-cloud@users.noreply.github.com>
2022-12-29 17:40:03 +0530

    test

--- dynamicsgrule.sh
+++ dynamicsgrule.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ip=$(ec2-metadata -v | awk '{print $2}')
+#comment
 
 echo $ip
 #aws ec2 modify-security-group-rules --group-id sg-05d60a27767105b58  --security-group-rules SecurityGroupRuleId=sgr-03f3344d3fe370e00,SecurityGroupRule='{Description='ansible-ip',IpProtocol=-1,CidrIpv4='$ip'/32}'
